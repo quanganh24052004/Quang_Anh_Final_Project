c66a2caf70a45299e22b21abac6826f643f23578
Nguyễn Quang Anh <anh.nguyenquang24052004@gmail.com>
2025-07-04 08:46:34 +0700

    Initial Commit

---
+++ Quang_Anh_Final_Project_UseXib/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quang_Anh_Final_Project_UseXib
+//
+//  Created by Nguyễn Quang Anh on 4/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
