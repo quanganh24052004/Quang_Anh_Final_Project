d5fa20b6bf5768381836a39a9c7b758f0b6d1571
quanganh24052004 <anh.nguyenquang24052004@gmail.com>
2025-07-07 00:00:31 +0700

    Premium Button

---
+++ Quang_Anh_Final_Project_UseXib/View/CustomView/PremiumCustomView.swift
@@ -0,0 +1,20 @@
+//
+//  PremiumCustomView.swift
+//  Quang_Anh_Final_Project_UseXib
+//
+//  Created by iKame Elite Fresher 2025 on 7/6/25.
+//
+
+import UIKit
+
+class PremiumCustomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
